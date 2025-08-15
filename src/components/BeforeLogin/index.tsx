94ce6e2bcb20e0be3bf0680b0a26e72a50e2ff62
Amrita Chanda <amrita.chanda@gmail.com>
2025-08-15 22:33:58 +0530

    Tidied up the BeforeLogin and BeforeDashboard to show only relevant info

--- src/components/BeforeLogin/index.tsx
+++ src/components/BeforeLogin/index.tsx
@@ -4,8 +4,13 @@ const BeforeLogin: React.FC = () => {
   return (
     <div>
       <p>
-        <b>Welcome to your dashboard!</b>
-        {' This is where site admins will log in to manage your website.'}
+        <b>Welcome to your content management system!</b>
+        {' This is where content editors will log in to manage the website. To test the CMS, the demo login credentials are:'}
+      </p>
+      <p>
+        <strong>Email:</strong>demo@uafanp.org
+        <br />
+        <strong>Password:</strong>demoadmin123
       </p>
     </div>
   )
