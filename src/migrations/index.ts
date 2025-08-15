93cb6d96b62cf2ca20289303633e08b83910199e
Amrita Chanda <amrita.chanda@gmail.com>
2025-08-15 18:46:29 +0530

    Fixed typescript errors in seed data.

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20241125_222020_initial from './20241125_222020_initial';
 import * as migration_20241214_124128 from './20241214_124128';
 import * as migration_20250815_114454_user_role from './20250815_114454_user_role';
 import * as migration_20250815_130832_user_avatar from './20250815_130832_user_avatar';
+import * as migration_20250815_131604_user_avatar2 from './20250815_131604_user_avatar2';
 
 export const migrations = [
   {
@@ -22,6 +23,11 @@ export const migrations = [
   {
     up: migration_20250815_130832_user_avatar.up,
     down: migration_20250815_130832_user_avatar.down,
-    name: '20250815_130832_user_avatar'
+    name: '20250815_130832_user_avatar',
+  },
+  {
+    up: migration_20250815_131604_user_avatar2.up,
+    down: migration_20250815_131604_user_avatar2.down,
+    name: '20250815_131604_user_avatar2'
   },
 ];
