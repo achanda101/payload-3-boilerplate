0411f43366a5a52d9ef90d55f5357f187170a66e
Amrita Chanda <amrita.chanda@gmail.com>
2025-08-22 16:08:09 +0530

    Changed adminThumbnail from thumbnail to small

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -38,7 +38,7 @@ export const Media: CollectionConfig = {
   upload: {
     // Upload to the public/media directory in Next.js making them publicly accessible even outside of Payload
     staticDir: path.resolve(dirname, '../../public/media'),
-    adminThumbnail: 'thumbnail',
+    adminThumbnail: 'small',
     imageSizes: [
       {
         name: 'thumbnail',
