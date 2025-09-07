6b19439e02e8fa6af7b7175e8b989d4b8cca2753
Amrita Chanda <amrita.chanda@gmail.com>
2025-09-08 04:33:31 +0530

    Fixed import for the SecondaryCTA component in PageContent.tsx

--- src/app/(frontend)/components/PageContent/PageContent.tsx
+++ src/app/(frontend)/components/PageContent/PageContent.tsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { useLanguage } from '@/providers/LanguageContext'
-import { SecondaryCTA } from './components/secondaryCTA'
+import { SecondaryCTA } from './components/SecondaryCTA'
 
 interface PageProps {
   data?: {
