b4c63a809666a71d1e9052aae89b5ff4986b0760
Amrita Chanda <amrita.chanda@gmail.com>
2025-11-13 04:31:32 +0530

    Added video tutorial section on dashboard

--- src/components/BeforeDashboard/index.tsx
+++ src/components/BeforeDashboard/index.tsx
@@ -9,11 +9,24 @@ const BeforeDashboard: React.FC = () => {
   return (
     <div className={baseClass}>
       <Banner className={`${baseClass}__banner`} type="success">
-        <h4>Welcome to your dashboard!</h4>
-        <br />
         <p>
           <Link href="/">View Homepage</Link>
         </p>
+        <div className='flex gap-1 backdrop-brightness-50'>
+          <h3>Video Tutorials</h3>
+          <div className='flex flex-col gap-1'>
+            <p>
+              <Link href="https://www.youtube.com/watch?v=aDb8VjdZkvw" target='blank'>
+                How to create a Page
+              </Link>
+            </p>
+            <p>
+              <Link href="#" target='blank'>
+                How to create a Grant Page
+              </Link>
+            </p>
+          </div>
+        </div>
       </Banner>
       
     </div>
