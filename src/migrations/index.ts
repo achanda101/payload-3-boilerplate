0cf3b9e4acfda1f7fa53ee2516b890a3043544f9
Amrita Chanda <amrita.chanda@gmail.com>
2025-12-17 04:23:11 +0530

    fix: added migration for new block pinkpuffy

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251216_181935_fresh_prod from './20251216_181935_fresh_prod';
+import * as migration_20251216_225254_pinkpuffy from './20251216_225254_pinkpuffy';
 
 export const migrations = [
   {
     up: migration_20251216_181935_fresh_prod.up,
     down: migration_20251216_181935_fresh_prod.down,
-    name: '20251216_181935_fresh_prod'
+    name: '20251216_181935_fresh_prod',
+  },
+  {
+    up: migration_20251216_225254_pinkpuffy.up,
+    down: migration_20251216_225254_pinkpuffy.down,
+    name: '20251216_225254_pinkpuffy'
   },
 ];
